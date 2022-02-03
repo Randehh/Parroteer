188a02772963cf8800cf063033b98b17eedfe868
Rondoodoo <randy.schouten@outlook.com>
2022-02-03 22:42:27 +0000

    Cleanup of twitter scraping file

--- Parroteer/DataSources/DataSourceTwitterScrape.cs
+++ Parroteer/DataSources/DataSourceTwitterScrape.cs
@@ -14,12 +14,17 @@ using System.Threading.Tasks;
 
 namespace Parroteer.DataSources {
 	public class DataSourceTwitterScrape : DataSourceBase {
+		public const string TwitterSearchUrlTemplate = @"https://twitter.com/search?q=from%3A{0}%20since%3A{1}%20until%3A{2}&f=live";
+		public const string GetTweetLinksFunctionTemplate = @"Array.from(document.querySelectorAll('a')).map(a => a.href).filter(a => a.startsWith('{0}')).filter(a => a.includes('/photo/') == false);";
+		public const int PageRetryCount = 10;
+
 		public override DataSourceTypes SourceType => DataSourceTypes.TWITTER_SCRAPER;
 		public override IDataSourceSerializer Serializer { get; set; }
 		public override string FileName => "Twitter_Scraped.json";
-		private string TweetLinkFormat => String.Format(@"https://twitter.com/{0}/status/", SourceID);
+		private string TweetLinkFormat => string.Format(@"https://twitter.com/{0}/status/", SourceID);
+		private string TweetLinksFunction => string.Format(GetTweetLinksFunctionTemplate, TweetLinkFormat);
 
-		private int m_ScraperCount = 8;
+		private int m_ScraperCount = 5;
 		public int ScraperCount {
 			get => m_ScraperCount;
 			set => SetProperty(ref m_ScraperCount, value);
@@ -86,62 +91,64 @@ namespace Parroteer.DataSources {
 			await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
 			using (Browser browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true })) {
 				using (Page page = await browser.NewPageAsync()) {
-					string baseUrl = @"https://twitter.com/search?q=from%3A{0}%20since%3A{1}%20until%3A{2}&f=live";
+					await ScrapeUserTask(page, datetimeProvider);
+				}
+				await browser.CloseAsync();
+			}
+		}
 
-					DateTimeOffset startRangeHigh;
-					DateTimeOffset startRangeLow;
-					datetimeProvider.GetNextDateTime(out startRangeHigh, out startRangeLow);
-					while (startRangeHigh > datetimeProvider.SinceTime) {
-						string currentUrl = string.Format(baseUrl, SourceID, startRangeLow.ToString("yyyy-MM-dd"), startRangeHigh.ToString("yyyy-MM-dd"));
+		private async Task ScrapeUserTask(Page page, TwitterScrapeDateProvider datetimeProvider) {
+			DateTimeOffset startRangeHigh;
+			DateTimeOffset startRangeLow;
+			datetimeProvider.GetNextDateTime(out startRangeHigh, out startRangeLow);
+			while (startRangeHigh > datetimeProvider.SinceTime) {
+				string currentUrl = string.Format(TwitterSearchUrlTemplate, SourceID, startRangeLow.ToString("yyyy-MM-dd"), startRangeHigh.ToString("yyyy-MM-dd"));
 
-						await page.GoToAsync(currentUrl);
+				await page.GoToAsync(currentUrl);
 
-						string lastTweet = "";
-						string tweetsFilter = $@"Array.from(document.querySelectorAll('a')).map(a => a.href).filter(a => a.startsWith('{TweetLinkFormat}')).filter(a => a.includes('/photo/') == false);";
-						int retryCount = 10;
+				string lastTweet = "";
 
-						while (true) {
-							string[] urls = new string[0];
-							for (int i = 0; i < retryCount; i++) {
-								urls = await page.EvaluateExpressionAsync<string[]>(tweetsFilter);
-								if (urls.Length != 0 && urls[urls.Length - 1] != lastTweet) break;
-								await Task.Delay(1000);
-							}
+				while (true) {
+					string[] urls = new string[0];
+					for (int i = 0; i < PageRetryCount; i++) {
+						urls = await page.EvaluateExpressionAsync<string[]>(TweetLinksFunction);
+						if (urls.Length != 0 && urls[urls.Length - 1] != lastTweet) break;
+						await page.EvaluateExpressionAsync("window.scrollBy(0, 1000)");
+						await Task.Delay(1000);
+					}
 
-							if (urls.Length == 0) {
-								// Next datetime, no tweets on this day
-								await page.ScreenshotAsync($"Resources/{startRangeLow:yyyy-MM-dd} to {startRangeHigh:yyyy-MM-dd}.png");
-								break;
-							} else {
-								if (urls[urls.Length - 1] == lastTweet) {
-									// Last tweet has been reached
-									await page.ScreenshotAsync($"Resources/{startRangeLow:yyyy-MM-dd} to {startRangeHigh:yyyy-MM-dd}.png");
-									break;
-								}
-								lastTweet = urls[urls.Length - 1];
-
-								foreach (string tweetUrl in urls) {
-									string tweetIdString = tweetUrl.Split('/')[5];
-									if (tweetIdString.Contains("?")) {
-										tweetIdString = tweetIdString.Split('?')[0];
-									}
-									long tweetId;
-									if (long.TryParse(tweetIdString, out tweetId) && m_TweetsFound.Add(tweetId)) {
-										m_TweetsToFetch.Add(tweetId);
-									}
-								}
-								await page.EvaluateExpressionAsync("window.scrollBy(0, 5000)");
-							}
-						}
-
-						if (datetimeProvider.HasNextDate()) {
-							datetimeProvider.GetNextDateTime(out startRangeHigh, out startRangeLow);
-						} else {
+					if (urls.Length == 0) {
+						// Next datetime, no tweets on this day
+						break;
+					} else {
+						if (urls[urls.Length - 1] == lastTweet) {
+							// Last tweet has been reached
 							break;
+						}
+						lastTweet = urls[urls.Length - 1];
+
+						foreach (string tweetUrl in urls) {
+							ProcessTweetUrl(tweetUrl);
 						}
 					}
 				}
-				await browser.CloseAsync();
+
+				if (datetimeProvider.HasNextDate()) {
+					datetimeProvider.GetNextDateTime(out startRangeHigh, out startRangeLow);
+				} else {
+					break;
+				}
+			}
+		}
+
+		private void ProcessTweetUrl(string tweetUrl) {
+			string tweetIdString = tweetUrl.Split('/')[5];
+			if (tweetIdString.Contains("?")) {
+				tweetIdString = tweetIdString.Split('?')[0];
+			}
+			long tweetId;
+			if (long.TryParse(tweetIdString, out tweetId) && m_TweetsFound.Add(tweetId)) {
+				m_TweetsToFetch.Add(tweetId);
 			}
 		}
 
@@ -150,23 +157,29 @@ namespace Parroteer.DataSources {
 			int retryCount = 3;
 			foreach (long tweetId in m_TweetsToFetch.GetConsumingEnumerable()) {
 				batch.Add(tweetId);
-				if (batch.Count == 100) {
-					for (int i = 0; i < retryCount; i++) {
-						try {
-							CoreTweet.Core.DictionaryResponse<string, Status> result = m_Session.Statuses.LookupMap(batch, trim_user: true, include_entities: false, include_ext_alt_text: false, tweet_mode: TweetMode.Extended);
-							foreach (Status tweet in result.Values) {
-								if (tweet != null) {
-									string sanitizedTweet = SanitizeTweet(tweet.FullText);
-									if (string.IsNullOrEmpty(sanitizedTweet)) continue;
+				if (batch.Count != 100) {
+					continue;
+				}
 
-									m_SyncContext.Send((_) => {
-										DataLines.Add(sanitizedTweet);
-									}, null);
-								}
-							}
-						} catch (Exception) { }
-					}
-					batch.Clear();
+				for (int i = 0; i < retryCount; i++) {
+					try { // In case Twitter API service errors
+						SendBatchToTwitterApi(batch);
+					} catch (Exception) { }
+				}
+				batch.Clear();
+			}
+		}
+
+		private void SendBatchToTwitterApi(List<long> batch) {
+			CoreTweet.Core.DictionaryResponse<string, Status> result = m_Session.Statuses.LookupMap(batch, trim_user: true, include_entities: false, include_ext_alt_text: false, tweet_mode: TweetMode.Extended);
+			foreach (Status tweet in result.Values) {
+				if (tweet != null) {
+					string sanitizedTweet = SanitizeTweet(tweet.FullText);
+					if (string.IsNullOrEmpty(sanitizedTweet)) continue;
+
+					m_SyncContext.Send((_) => {
+						DataLines.Add(sanitizedTweet);
+					}, null);
 				}
 			}
 		}
@@ -179,11 +192,14 @@ namespace Parroteer.DataSources {
 			tweet = Regex.Replace(tweet, @"http[^\s]+", "");        // Hyperlinks
 			tweet = Regex.Replace(tweet, @"[@]\w+ ", "");           // @ handles
 			tweet = Regex.Replace(tweet, @"[^\u0000-\u007F]+", ""); // Emojis
-			tweet = tweet.Replace(" .", ".");
+			tweet = tweet.Replace(" .", ".");						// Spaces left by emojis (maybe)
 			tweet = tweet.Trim();
 			return tweet;
 		}
 
+		/// <summary>
+		/// Provides a class from which multiple scrapers can get the next datetime to scrape
+		/// </summary>
 		private class TwitterScrapeDateProvider {
 			public event EventHandler OnNextDateTimeRequested;
 
